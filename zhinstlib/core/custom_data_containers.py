07f0b34dd4f9d4876e3811414415269668efc379
pikestefan <lucio.stefan@gmail.com>
2022-02-16 10:34:17 +0100

    Created custom data containers
    Two containers two store the lock-in data

---
+++ zhinstlib/core/custom_data_containers.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class LockinData(object):
+    def __init__(self):
+        self._demods = dict()
+
+    def create_demod(self, demod, time_axis, x_quad, y_quad, frequency):
+        data_container = DemodulatorDataContainer(time_axis=time_axis, x_quad=x_quad, y_quad=y_quad,
+                                                  frequency=frequency)
+        data_container.set_circular_coords()
+
+    def get_demods(self):
+        return np.array(self._demods.keys())
+
+    def demod(self, demod):
+        return self._demods[demod]
+
+
+
+class DemodulatorDataContainer(object):
+    def __init__(self, time_axis = None, x_quad = None, y_quad = None, frequency = None):
+        self.x_quad = x_quad
+        self.y_quad = y_quad
+        self.frequency = frequency
+        self.time_axis = time_axis
+
+        self.r, self.phase = None, None
+
+    def set_circular_coords(self, x_quad=None, y_quad=None):
+        if x_quad is not None and y_quad is not None:
+            complex_amp = x_quad + 1j*y_quad
+        else:
+            complex_amp = self.x_quad + 1j*self.y_quad
+        r = np.abs(complex_amp)
+        phase = np.angle(complex_amp)
+        self.r, self.phase = r, phase
+
