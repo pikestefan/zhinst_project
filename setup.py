0db45ff6134b172bc78e2d7abf5fc59f2c56bdd8
pikestefan <lucio.stefan@gmail.com>
2022-02-16 13:59:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name='zhinstlib', version='1.0', packages=find_packages())
+from setuptools import setup
+
+setup(
+    name='zhinstlib',
+    version='1.0',
+    packages=find_packages('src'),
+)
