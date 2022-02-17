f190e189ff8f0ed39302f37cece7a38fbd67935e
pikestefan <lucio.stefan@gmail.com>
2022-02-17 18:14:09 +0100

    Update mouse_interacting_lineedit.py
    Added stylesheets for the different chip statuses.

--- zhinstlib/custom_widgets/mouse_interacting_lineedit.py
+++ zhinstlib/custom_widgets/mouse_interacting_lineedit.py
@@ -11,10 +11,18 @@ class InteractingLineEdit(QTextEdit):
         self.setSizePolicy( QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed) )
         self.fixed_size = fixed_size
         self.setFixedSize(self.fixed_size, self.fixed_size)
-        self.stylesheets = {"no hover": "background-color:rgb(204,204,204);border: 1px solid black;",
-                            "hover": "background-color:rgb(204,204,204);border: 3px solid red;",
-                            "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49);"}
-        self.setStyleSheet(self.stylesheets["no hover"])
+        self._stylesheet_modes = {"standard": {"no hover": "background-color:rgb(204,204,204);border: 1px solid black;",
+                                              "hover": "background-color:rgb(204,204,204);border: 3px solid red;",
+                                              "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49);"},
+                                 "data loaded": {"no hover": "background-color:rgb(204,204,204);border: 1px solid rgb(0,118,19);",
+                                                 "hover": "background-color:rgb(204,204,204);border: 3px solid rgb(0,118,19);",
+                                                 "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49);"},
+                                 "data missing": {"no hover": "background-color:rgb(150,150,150);border: 1px solid black;",
+                                                  "hover": "background-color:rgb(204,204,204);border: 3px solid red;",
+                                                  "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49);"},
+                                 }
+        self._active_stylesheet = self._stylesheet_modes["standard"]
+        self.setStyleSheet(self._active_stylesheet["no hover"])
         self.isclicked = False
         self._interaction_enabled = True
         self.installEventFilter(self)
@@ -22,23 +30,33 @@ class InteractingLineEdit(QTextEdit):
     def eventFilter(self, source, event):
         if (source is self) and self._interaction_enabled:
             if event.type() == QEvent.Enter and not self.isclicked:
-                self.setStyleSheet(self.stylesheets["hover"])
+                self.setStyleSheet(self._active_stylesheet["hover"])
             elif event.type() == QEvent.Leave and not self.isclicked:
-                self.setStyleSheet(self.stylesheets["no hover"])
+                self.setStyleSheet(self._active_stylesheet["no hover"])
             elif event.type() == QEvent.MouseButtonPress:
                 if not self.isclicked:
                     self.isclicked = True
-                    self.setStyleSheet(self.stylesheets["clicked"])
+                    self.setStyleSheet(self._active_stylesheet["clicked"])
                 else:
                     self.isclicked = False
-                    self.setStyleSheet(self.stylesheets["hover"])
+                    self.setStyleSheet(self._active_stylesheet["hover"])
                 self.signal_widget_clicked.emit(self.id)
         return QWidget.eventFilter(self, source, event)
 
     def unclick(self):
         self.isclicked = False
-        self.setStyleSheet(self.stylesheets["no hover"])
+        self.setStyleSheet(self._active_stylesheet["no hover"])
         self.signal_widget_clicked.emit(self.id)
 
     def interaction_enabled(self, value):
         self._interaction_enabled = value
+
+    def set_active_stylesheet(self, style_name):
+        if style_name not in self._stylesheet_modes.keys():
+            print("Invalid style list")
+        else:
+            self._active_stylesheet = self._stylesheet_modes[style_name]
+            if self.isclicked:
+                self.setStyleSheet(self._active_stylesheet["clicked"])
+            else:
+                self.setStyleSheet(self._active_stylesheet["no hover"])
