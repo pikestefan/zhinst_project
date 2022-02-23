2b14227167065b5f2fd070769e17e164201de80e
pikestefan <lucio.stefan@gmail.com>
2022-02-23 12:11:58 +0100

    Update graph_widget_selbox.py
    Made the graph to draw squares and reset the range accordingly.

--- zhinstlib/custom_widgets/graph_widget_selbox.py
+++ zhinstlib/custom_widgets/graph_widget_selbox.py
@@ -1,76 +1,98 @@
 import sys
+
+import pyqtgraph
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import pyqtSignal
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph import PlotWidget, plot
 
-# *********************************************************************************************
-# *********************************************************************************************
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+class SelectableAreaPlotWidget(pg.PlotWidget):
+    areaSelectedSignal = pyqtSignal(float, float)
 
-        self.setWindowTitle("My MainWindow")
-        self.qPlotWidget = pg.PlotWidget(self)
-        self.qPlotWidget.setLabel("bottom", "X-Axis")
-        self.qPlotWidget.setLabel("left", "Y-Axis")
-        self.qPlotWidget.scene().sigMouseClicked.connect(self.mouseClickedEvent)
+    def __init__(self, *args, **kwargs):
+        kwargs['viewBox'] = TestView()
+        super(SelectableAreaPlotWidget, self).__init__(*args, **kwargs)
+        #self.blo = pg.PlotDataItem(np.array([0,1,2]), np.square(np.array([0,1,2])))
+        #self.addItem(self.blo)
+        self.rect = None
 
-        data1 = np.zeros((2, 2), float) # create the array to hold the data
-        data1[0] = np.array((1.0, 10.0))
-        data1[1] = np.array((2.0, 20.0))
+        self.getViewBox().signalUpdateRect.connect(self.drawRect)
+        self.getViewBox().signalClearRect.connect(self.hideRect)
 
-        pen1 = pg.mkPen(color=(255,0,0), width=1) # red
-        self.qPlotWidget.plot(data1, pen=pen1, name="data1")
+    def drawRect(self, qrectf):
+        if self.rect is None:
+            self.getViewBox().disableAutoRange()
+            self.rect = pg.QtGui.QGraphicsRectItem(qrectf)
+            self.rect.setPen(pg.mkPen(None))
+            self.rect.setBrush(pg.mkBrush(color=(219, 212, 0, 80)))
+            self.addItem(self.rect)
+        self.rect.setRect(qrectf)
 
-    def mouseClickedEvent(self, event):
-        print("mouseClickedEvent")
-        pos = event.scenePos()
-        if (self.qPlotWidget.sceneBoundingRect().contains(pos)):
-            mousePoint = self.qPlotWidget.plotItem.vb.mapSceneToView(pos)
-            print("mousePoint=", mousePoint)
+    def hideRect(self, xrange):
+        self.removeItem(self.rect)
+        self.rect = None
+        self.getViewBox().enableAutoRange()
+        self.setXRange(xrange[0], xrange[1])
 
-            # draw and fill a 2-pixel by 2-pixel red rectangle where
-            # the mouse was clicked at [mousePoint.x(), mousePoint.y()]
-            # ??? add code here
+class TestView(pg.ViewBox):
+    signalUpdateRect = pyqtSignal(QtCore.QRectF)
+    signalClearRect = pyqtSignal(list)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start = None
+        self.viewHeight = None
+        self.y0 = None
 
-    def resizeEvent(self, event):
-        size = self.geometry()
-        self.qPlotWidget.setGeometry(10, 10, size.width()-20, size.height()-20)
+    def mouseDragEvent(self, ev, axis=None):
+        if ev.button() == QtCore.Qt.LeftButton:
+            ev.accept()
+            if ev.isStart():
+                #TODO: fix this, it is cleaner to get the coordinate at the beginning of drag with the ev.buttonDown() or
+                # something along those lines
+                self.start = self.mapToView(ev.pos())
+                _, yviewrange = self.viewRange()
+                self.y0 = yviewrange[0]
+                self.viewHeight = yviewrange[1] - self.y0
+            else:
+                keymods = QtWidgets.QApplication.keyboardModifiers()
+                currpos = self.mapToView(ev.pos())
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(30,30,600,400)
-        self.begin = QtCore.QPoint()
-        self.end = QtCore.QPoint()
-        self.show()
+                #Negative widths and heights cause issues in the correct displaying of images
+                x0 = self.start.x() if self.start.x() <= currpos.x() else currpos.x()
+                width = abs(currpos.x() - self.start.x())
 
-    def paintEvent(self, event):
-        qp = QtGui.QPainter(self)
-        br = QtGui.QBrush(QtGui.QColor(100, 10, 10, 40))
-        qp.setBrush(br)
-        qp.drawRect(QtCore.QRect(self.begin, self.end))
+                #Make a plot-tall selection if you hold shift and drag
+                if keymods and QtCore.Qt.ShiftModifier:
+                    height = self.viewHeight
+                    y0 = self.y0
+                #Otherwise, just draw a square
+                else:
+                    height = abs(currpos.y() - self.start.y())
+                    y0 = self.start.y() if self.start.y() <= currpos.y() else currpos.y()
+                self.signalUpdateRect.emit(QtCore.QRectF(x0, y0, width, height))
+            if ev.isFinish():
+                #TODO: at the moment, the signal emits only the x range. Send also the y range, and use it
+                # to rescale the plot when dragging the square selection, while do nothing if pressing shift
+                xpositions = [self.start.x(), self.mapToView(ev.pos()).x()]
+                xpositions.sort()
+                self.signalClearRect.emit(xpositions)
+        else:
+            super().mouseDragEvent(ev)
 
-    def mousePressEvent(self, event):
-        self.begin = event.pos()
-        self.end = event.pos()
-        self.update()
-
-    def mouseMoveEvent(self, event):
-        self.end = event.pos()
-        self.update()
-
-    def mouseReleaseEvent(self, event):
-        self.begin = event.pos()
-        self.end = event.pos()
-        self.update()
+    def mouseDoubleClickEvent(self, ev, axis=None):
+        if ev.button() == QtCore.Qt.LeftButton:
+            ev.accept()
+            self.autoRange()
+        else:
+            super().mouseDoubleClickEvent()
 
 if __name__ == '__main__':
+
     app = QtWidgets.QApplication(sys.argv)
 
-    w = MainWindow()
+    w = SelectableAreaPlotWidget()
     screen = QtWidgets.QDesktopWidget().screenGeometry()
     w.setGeometry(100, 100, screen.width()-200, screen.height()-200) # x, y, Width, Height
     w.show()
