6513db4f7c459b52cadebeb69f087db82adae859
pikestefan <lucio.stefan@gmail.com>
2022-02-23 18:39:38 +0100

    Update graph_widget_selbox.py
    Added a custom plotdataitem subclass that resamples the data in case the dataset is too big.

--- zhinstlib/custom_widgets/graph_widget_selbox.py
+++ zhinstlib/custom_widgets/graph_widget_selbox.py
@@ -5,14 +5,14 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import pyqtSignal
 import numpy as np
 import pyqtgraph as pg
-from pyqtgraph import PlotWidget, plot
+from pyqtgraph import PlotWidget, PlotDataItem
 
 
 class SelectableAreaPlotWidget(pg.PlotWidget):
     areaSelectedSignal = pyqtSignal(float, float)
 
     def __init__(self, *args, **kwargs):
-        kwargs['viewBox'] = TestView()
+        kwargs['viewBox'] = ViewForBoxDrag()
         super(SelectableAreaPlotWidget, self).__init__(*args, **kwargs)
         #self.blo = pg.PlotDataItem(np.array([0,1,2]), np.square(np.array([0,1,2])))
         #self.addItem(self.blo)
@@ -34,9 +34,48 @@ class SelectableAreaPlotWidget(pg.PlotWidget):
         self.removeItem(self.rect)
         self.rect = None
         self.getViewBox().enableAutoRange()
-        self.setXRange(xrange[0], xrange[1])
+        self.setXRange(xrange[0], xrange[1], padding=0)
 
-class TestView(pg.ViewBox):
+class DownsamplerPlotDataItem(PlotDataItem):
+    """
+    A custom PlotDataItem that resamples the data, to increase the plotting speed.
+    """
+    def __init__(self, max_data = 1e20, *args, **kwargs):
+        self.max_data = max_data
+
+        if len(args) == 2:
+            x_ax = args[0]
+            y_ax = args[1]
+            if len(x_ax) > self.max_data:
+                args = self.resample_data(x_ax, y_ax)
+        if 'x' in kwargs and 'y' in kwargs:
+            x_ax = kwargs['x']
+            y_ax = kwargs['y']
+            if len(x_ax) > self.max_data:
+                kwargs['x'], kwargs['y'] = self.resample_data(x_ax, y_ax)
+
+        super(DownsamplerPlotDataItem, self).__init__(*args, **kwargs)
+
+    def resample_data(self, x, y):
+        new_x = np.linspace(x[0], x[-1], self.max_data)
+        new_y = np.interp(new_x, x, y)
+        return new_x, new_y
+
+    def setData(self, *args, **kwargs):
+        if len(args) == 2:
+            x_ax = args[0]
+            y_ax = args[1]
+            if len(x_ax) > self.max_data:
+                args = self.resample_data(x_ax, y_ax)
+        if 'x' in kwargs and 'y' in kwargs:
+            x_ax = kwargs['x']
+            y_ax = kwargs['y']
+            if len(x_ax) > self.max_data:
+                kwargs['x'], kwargs['y'] = self.resample_data(x_ax, y_ax)
+
+        super().setData(*args,**kwargs)
+
+class ViewForBoxDrag(pg.ViewBox):
     signalUpdateRect = pyqtSignal(QtCore.QRectF)
     signalClearRect = pyqtSignal(list)
     def __init__(self, *args, **kwargs):
@@ -93,8 +132,13 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     w = SelectableAreaPlotWidget()
-    screen = QtWidgets.QDesktopWidget().screenGeometry()
-    w.setGeometry(100, 100, screen.width()-200, screen.height()-200) # x, y, Width, Height
+    x, y = np.linspace(0,100,100000), np.random.rand(100000)**2
+    sgnu = DownsamplerPlotDataItem(max_data=50000, pen = None, symbol = 'o', symbolPen = pg.mkPen('w'), symbolBrush = 'w', symbolSize = 3)
+    sgnu.setData(x, y)
+
+    w.addItem(sgnu)
+    #screen = QtWidgets.QDesktopWidget().screenGeometry()
+    #w.setGeometry(100, 100, screen.width()-200, screen.height()-200) # x, y, Width, Height
     w.show()
 
     sys.exit(app.exec_())
