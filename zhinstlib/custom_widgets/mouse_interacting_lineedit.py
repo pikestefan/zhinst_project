a2587b1b71c364ec7d27f0e69bd248f82ce37f87
pikestefan <lucio.stefan@gmail.com>
2022-03-01 17:45:30 +0100

    Revert "Update mouse_interacting_lineedit.py"
    This reverts commit 85bb46cbac17ea3c1f683ace8be7343f565a32cc.

--- zhinstlib/custom_widgets/mouse_interacting_lineedit.py
+++ zhinstlib/custom_widgets/mouse_interacting_lineedit.py
@@ -1,11 +1,9 @@
 from PyQt5.QtWidgets import QTextEdit,QSizePolicy, QWidget
 from PyQt5.QtCore import pyqtSignal, QEvent
-from PyQt5.QtCore import Qt
 
 class InteractingLineEdit(QTextEdit):
 
     signal_widget_clicked = pyqtSignal(str)
-    signal_show_text = pyqtSignal(str)
     def __init__(self, id, fixed_size = 80, *args, **kwargs):
         super(InteractingLineEdit, self).__init__(*args, **kwargs)
         self.id = id
@@ -14,16 +12,13 @@ class InteractingLineEdit(QTextEdit):
         self.fixed_size = fixed_size
         self.setFixedSize(self.fixed_size, self.fixed_size)
 
-        #This will set the base stylesheet
-        self.setStyleSheet("font-size:10px; color:k")
-
-        self._stylesheet_modes = {"standard": {"no hover": "background-color:rgb(204,204,204);border: 1px solid black;",
-                                              "hover": "background-color:rgb(204,204,204);border: 3px solid red",
-                                              "clicked" : "background-color:rgb(255,183,67);border: 3px solid rgb(0,148,49)"},
-                                 "data loaded": {"no hover": "background-color:rgb(155,225,199);border: 1px solid rgb(0,0,0)",
-                                                 "hover": "background-color:rgb(155,225,199);border: 3px solid rgb(0,118,19)",
-                                                 "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49)"},
-                                 "data missing": {"no hover": "background-color:rgb(150,150,150);border: 1px solid black"},
+        self._stylesheet_modes = {"standard": {"no hover": "background-color:rgb(204,204,204);border: 1px solid black; font-size:10px; color:k",
+                                              "hover": "background-color:rgb(204,204,204);border: 3px solid red; font-size:10px; color:k",
+                                              "clicked" : "background-color:rgb(255,183,67);border: 3px solid rgb(0,148,49); font-size:10px; color:k"},
+                                 "data loaded": {"no hover": "background-color:rgb(155,225,199);border: 1px solid rgb(0,0,0); font-size:10px; color:k",
+                                                 "hover": "background-color:rgb(155,225,199);border: 3px solid rgb(0,118,19); font-size:10px; color:k",
+                                                 "clicked" : "background-color:rgb(158,255,245);border: 3px solid rgb(0,148,49); font-size:10px; color:k"},
+                                 "data missing": {"no hover": "background-color:rgb(150,150,150);border: 1px solid black; font-size:10px; color:k"},
                                  }
         self._active_stylesheet = self._stylesheet_modes["standard"]
         self.setStyleSheet(self._active_stylesheet["no hover"])
@@ -38,7 +33,7 @@ class InteractingLineEdit(QTextEdit):
                 self.setStyleSheet(self._active_stylesheet["hover"])
             elif event.type() == QEvent.Leave and not self.isclicked:
                 self.setStyleSheet(self._active_stylesheet["no hover"])
-            elif event.type() == QEvent.MouseButtonPress and event.button() == Qt.LeftButton:
+            elif event.type() == QEvent.MouseButtonPress:
                 if not self.isclicked:
                     self.isclicked = True
                     self.setStyleSheet(self._active_stylesheet["clicked"])
@@ -46,8 +41,6 @@ class InteractingLineEdit(QTextEdit):
                     self.isclicked = False
                     self.setStyleSheet(self._active_stylesheet["hover"])
                 self.signal_widget_clicked.emit(self.id)
-            elif event.type() == QEvent.MouseButtonPress and event.button() == Qt.RightButton:
-                self.signal_show_text.emit(self.id)
         return QWidget.eventFilter(self, source, event)
 
     def set_interacting(self, value):
