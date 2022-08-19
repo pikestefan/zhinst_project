ed2b565f51289b54e318c2a0beb7b57e6036d06b
pikestefan <lucio.stefan@gmail.com>
2022-08-19 18:18:29 +0200

    Mods in core: created test classes of the zinstDevice and the wafer_analyzer, to support a better type of readout.

--- zhinstlib/core/zinst_device.py
+++ zhinstlib/core/zinst_device.py
@@ -1,5 +1,4 @@
-import zhinst.ziPython as zi
-import sys
+from zhinst.ziPython import ziDAQServer, ziListEnum
 import numpy as np
 from zhinstlib.helpers.helper_funcs import get_device_props
 import time
@@ -16,13 +15,14 @@ class ziVirtualDevice(object):
                     device_props['serverport'],
                     device_props['apilevel']]
 
-        self.ziServer = zi.ziDAQServer(*args, **kwargs)
+        self.ziServer = ziDAQServer(*args, **kwargs)
         self.dev_name = dev_name
-        self.__baseaddress = f'/{dev_name}'
+        self._baseaddress = f'/{dev_name}'
         self._demod_num = self.get_available_demods()
         self.demod_properties = dict().fromkeys([demod for demod in range(self._demod_num)])
-        self.clockbase = self.ziServer.getInt(self.__baseaddress + '/clockbase')
+        self.clockbase = self.ziServer.getInt(self._baseaddress + '/clockbase')
         self.daqmodules = dict()
+        self.daqmodules_sigs = dict()
         self._daqstopRequested = False
 
         for key in self.demod_properties.keys():
@@ -42,7 +42,7 @@ class ziVirtualDevice(object):
             self._hasmods = True
 
     def get_device_nodes(self):
-        nodes = self.ziServer.listNodes(self.__baseaddress)
+        nodes = self.ziServer.listNodes(self._baseaddress)
 
         node_names = [node.split('/')[-1] for node in nodes]
 
@@ -63,7 +63,7 @@ class ziVirtualDevice(object):
         :param kwargs: passed onto the poll method
         :return:
         """
-        cmd = self.__baseaddress + f'/demods/{demod}/sample'
+        cmd = self._baseaddress + f'/demods/{demod}/sample'
         self.ziServer.subscribe(cmd)
         self.ziServer.flush()
         data = self.ziServer.poll(duration, flat=True, timeout_ms=timeout_ms, *args, **kwargs)
@@ -79,7 +79,7 @@ class ziVirtualDevice(object):
 
     def set_pid_enabled(self, pid_num=0, enabled=False):
         if self._haspids:
-            cmd = self.__baseaddress + f'/pids/{pid_num}/enable'
+            cmd = self._baseaddress + f'/pids/{pid_num}/enable'
             self.ziServer.setInt(cmd, int(enabled))
         else:
             raise (Exception('Device has no PIDs'))
@@ -111,7 +111,7 @@ class ziVirtualDevice(object):
         if module_name is None:
             module_name = f'daq{len(self.daqmodules)}'
 
-        demod_paths = [self.__baseaddress + f'/demods/{demod}/sample' for demod in demods]
+        demod_paths = [self._baseaddress + f'/demods/{demod}/sample' for demod in demods]
         sampling_rate = max([self.demod_properties[demod]['rate'] for demod in demods])
 
         cmd_list = []
@@ -119,8 +119,8 @@ class ziVirtualDevice(object):
             for sig in signal_list:
                 cmd_list.append(demod + f'.{sig}')
 
-        flags = zi.ziListEnum.recursive | zi.ziListEnum.absolute | zi.ziListEnum.streamingonly
-        streaming_nodes = self.ziServer.listNodes(self.__baseaddress, flags)
+        flags = ziListEnum.recursive | ziListEnum.absolute | ziListEnum.streamingonly
+        streaming_nodes = self.ziServer.listNodes(self._baseaddress, flags)
 
         daq_module = self.ziServer.dataAcquisitionModule()
         for demod_number, demod_path in zip(demods, demod_paths):
@@ -242,19 +242,19 @@ class ziVirtualDevice(object):
             self.daqmodules.pop(daq_module_name)
 
     def set_aux_offset(self, aux=0, offset=0):
-        cmd = self.__baseaddress + f'/auxouts/{aux}/offset'
+        cmd = self._baseaddress + f'/auxouts/{aux}/offset'
         self.ziServer.setDouble(cmd, offset)
 
     def set_demod_harmonic(self, demod=None, harmonic=0):
-        cmd = self.__baseaddress + f'/demods/{demod}/harmonic'
+        cmd = self._baseaddress + f'/demods/{demod}/harmonic'
         self.ziServer.setInt(cmd, harmonic)
 
     def set_demod_oscillator(self, demod=None, oscillator=0):
-        cmd = self.__baseaddress + f'/demods/{demod}/oscselect'
+        cmd = self._baseaddress + f'/demods/{demod}/oscselect'
         self.ziServer.setInt(cmd, oscillator)
 
     def set_demod_output(self, demod=None, on_state=False, column=0):
-        cmd = self.__baseaddress + f'/sigouts/{column}/enables/{demod}'
+        cmd = self._baseaddress + f'/sigouts/{column}/enables/{demod}'
         self.ziServer.setInt(cmd, int(on_state))
 
     def set_mod_phase(self, mod_choice='carrier', phase=0, which_mod=0):
@@ -269,17 +269,17 @@ class ziVirtualDevice(object):
                 signal_choice = f'sidebands/{mod_choice[-1]}'
             else:
                 signal_choice = 'carrier'
-            cmd = self.__baseaddress + f'/mods/{which_mod}/{signal_choice}/phaseshift'
+            cmd = self._baseaddress + f'/mods/{which_mod}/{signal_choice}/phaseshift'
             self.ziServer.setDouble(cmd, phase)
         else:
             raise (Exception('Device has no MOD addon.'))
 
     def set_oscillator_freq(self, oscillator=None, frequency=None):
-        cmd = self.__baseaddress + f'/oscs/{oscillator}/freq'
+        cmd = self._baseaddress + f'/oscs/{oscillator}/freq'
         self.ziServer.setDouble(cmd, frequency)
 
     def set_output_range(self, output_channel=None, max_voltage=1):
-        cmd = self.__baseaddress + f'/sigouts/{output_channel}/range'
+        cmd = self._baseaddress + f'/sigouts/{output_channel}/range'
         self.ziServer.setDouble(cmd, max_voltage)
 
         actual_range = self.get_output_range(output_channel)
@@ -287,42 +287,42 @@ class ziVirtualDevice(object):
             print(f"Required voltage not available. Set max range to {actual_range}V instead.")
 
     def set_output_state(self, output_channel=None, on_state=False):
-        cmd = self.__baseaddress + f'/sigouts/{output_channel}/on'
+        cmd = self._baseaddress + f'/sigouts/{output_channel}/on'
         self.ziServer.setInt(cmd, int(on_state))
 
     def set_output_volt(self, demod=None, voltage=0, column=0):
-        cmd = self.__baseaddress + f'/sigouts/{column}/amplitudes/{demod}'
+        cmd = self._baseaddress + f'/sigouts/{column}/amplitudes/{demod}'
         multiplier = self.get_output_range(column)
         if voltage > multiplier:
             raise Warning("The required voltage exceeds the max output range. Clipping to the maximum.")
         self.ziServer.setDouble(cmd, voltage / multiplier)
 
     def set_demod_phase(self, demod=None, phase=0):
-        cmd = self.__baseaddress + f'/demods/{demod}/phaseshift'
+        cmd = self._baseaddress + f'/demods/{demod}/phaseshift'
         self.ziServer.setDouble(cmd, phase)
 
     def get_available_demods(self):
-        return len(self.ziServer.listNodes(self.__baseaddress + '/demods'))
+        return len(self.ziServer.listNodes(self._baseaddress + '/demods'))
 
     def get_aux_offset(self, aux=0):
-        cmd = self.__baseaddress + f'/auxouts/{aux}/offset'
+        cmd = self._baseaddress + f'/auxouts/{aux}/offset'
         offset = self.ziServer.getDouble(cmd)
         return offset
 
     def get_demod_harmonic(self, demod=None):
-        cmd = self.__baseaddress + f'/demods/{demod}/harmonic'
+        cmd = self._baseaddress + f'/demods/{demod}/harmonic'
         return self.ziServer.getInt(cmd)
 
     def get_demod_oscillator(self, demod=None):
-        cmd = self.__baseaddress + f'/demods/{demod}/oscselect'
+        cmd = self._baseaddress + f'/demods/{demod}/oscselect'
         return self.ziServer.getInt(cmd)
 
     def get_demod_output(self, demod=None, column=0):
-        cmd = self.__baseaddress + f'/sigouts/{column}/enables/{demod}'
+        cmd = self._baseaddress + f'/sigouts/{column}/enables/{demod}'
         return bool(self.ziServer.getInt(cmd))
 
     def get_demod_settings(self, demod=None):
-        props = self.ziServer.listNodes(self.__baseaddress + f'/demods/{demod}', settingsonly=True)
+        props = self.ziServer.listNodes(self._baseaddress + f'/demods/{demod}', settingsonly=True)
         prop_dictionary = dict()
         for prop in props:
             value = self.ziServer.getDouble(prop)
@@ -331,97 +331,20 @@ class ziVirtualDevice(object):
         return prop_dictionary
 
     def get_oscillator_freq(self, oscillator=None):
-        cmd = self.__baseaddress + f'/oscs/{oscillator}/freq'
+        cmd = self._baseaddress + f'/oscs/{oscillator}/freq'
         return self.ziServer.getDouble(cmd)
 
     def get_output_range(self, output_channel=None):
-        cmd = self.__baseaddress + f'/sigouts/{output_channel}/range'
+        cmd = self._baseaddress + f'/sigouts/{output_channel}/range'
         return self.ziServer.getDouble(cmd)
 
     def get_output_state(self, output_channel=None):
-        cmd = self.__baseaddress + f'/sigouts/{output_channel}/on'
+        cmd = self._baseaddress + f'/sigouts/{output_channel}/on'
         return bool(self.ziServer.getInt(cmd))
 
     def get_demod_phase(self, demod=None):
-        cmd = self.__baseaddress + f'/demods/{demod}/phaseshift'
+        cmd = self._baseaddress + f'/demods/{demod}/phaseshift'
         return self.ziServer.getDouble(cmd)
-
-    def optimize_freq(self, demod=None, wait_time=None, min_delta=1e-6, stepsize=0.5, itermax=1000, gain_factor=2,
-                      integration_time=0.1):
-        # TODO:estimate wait_time and poll integration time from Q factor
-        poll_kwargs = {'timeout_ms': 500,
-                       'flags': 0}
-
-        freq = self.get_oscillator_freq(demod)
-
-        ii = 0
-        prev_gradient = np.nan
-        contrast = 2 * min_delta
-        gain = 1
-
-        curr_amp = self.poll_demod_stream(demod, integration_time, return_amp=True, **poll_kwargs).mean()
-        direction = 1
-        while abs(contrast) > min_delta and ii < itermax:
-            new_freq = freq + direction * gain * stepsize
-            self.set_oscillator_freq(demod, new_freq)
-            time.sleep(wait_time)
-            new_amp = self.poll_demod_stream(demod, integration_time, return_amp=True, **poll_kwargs).mean()
-
-            gradient = (new_amp - curr_amp)
-
-            if gradient < 0:
-                direction *= -1
-
-            reduce_gain = (gradient < 0) and (prev_gradient >= 0)
-
-            if reduce_gain:
-                gain /= gain_factor
-
-            if not reduce_gain:
-                freq = new_freq
-                curr_amp = new_amp
-            else:
-                self.set_oscillator_freq(demod, freq)
-                time.sleep(wait_time)
-                curr_amp = self.poll_demod_stream(demod, integration_time, return_amp=True, **poll_kwargs).mean()
-
-            prev_gradient = gradient
-            contrast = gradient / (new_amp + curr_amp)
-            ii += 1
-
-        if ii == itermax:
-            print("Max. iteration number reached.")
-        else:
-            print("Converged")
-
-    def sync(self):
-        self.ziServer.sync()
-
-    def sweep_freq_and_optimize(self, demod=None, settle_time=None, int_time=None,
-                                coarse_range=5, fine_range=20e-3, coarse_steps=5, fine_steps=6):
-
-        # TODO:estimate wait_time and poll integration time from Q factor
-        poll_kwargs = {'timeout_ms': 500,
-                       'flags': 0}
-
-        for range in [coarse_range, fine_range]:
-            freq0 = self.get_oscillator_freq(demod)
-            freq_half_range = range / 2
-            if range == coarse_range:
-                steps = coarse_steps
-            else:
-                steps = fine_steps
-            freq_steps = np.linspace(freq0 - freq_half_range, freq0 + freq_half_range, steps)
-            measured_amps = np.zeros(freq_steps.shape)
-
-            for ii, freq in enumerate(freq_steps):
-                self.set_oscillator_freq(demod, freq)
-                time.sleep(settle_time)
-                curr_amp = self.poll_demod_stream(demod, int_time, return_amp=True, **poll_kwargs).max()
-                measured_amps[ii] = curr_amp
-            max_freq = freq_steps[np.argmax(measured_amps)]
-
-        return max_freq
 
 class PyQtziVirtualDevice(ziVirtualDevice, QObject):
 
@@ -432,13 +355,131 @@ class PyQtziVirtualDevice(ziVirtualDevice, QObject):
 
         self.signal_stop_daq.connect(self.request_stop)
 
-if __name__ == '__main__':
+class moddedziVirtualDevice(ziVirtualDevice):
+    def __init__(self, dev_name='', auto_properties=True, *args, **kwargs):
+        super(moddedziVirtualDevice,self).__init__(dev_name=dev_name, auto_properties=auto_properties, *args, **kwargs)
 
-    #import matplotlib.pyplot as plt
+    def set_subscribe_daq(self, demod_dictionary = dict(), read_duration=1., burst_duration=1.,
+                          module_name=None, daq_type='continous', interp_method='linear', autosaving_file_path = None,
+                          autosave_file_fmt='hdf5', triggernode = "", **kwargs):
+        """
+        Method that creates a daq module, adding it to the dictionary of the class daq modules. This method also sets
+        the daq module settings. Returns the name of the module as specified in the daq modules dictionary, and a
+        dictionary with the subscribed signals as keys. The dictionary items are empty lists.
+        """
+        filefmt_dictionary = {'matlab': 0, 'csv': 1, 'xsm': 3, 'hdf5': 4}
+        daq_type_dict = {'continous': 0, 'edge': 1, 'pulse': 3, 'tracking': 4,
+                    'digital': 2, 'hardware': 6, 'pulse counter': 8}
+        interp_method_dict = {'nearest': 1, 'linear': 2, None: 4}
 
-    device_id = 'dev5152'
+        cmd_string = self._baseaddress + "/demods/{:d}/sample.{}"
 
-    #props = get_device_props(device_id)
+        if daq_type not in daq_type_dict:
+            raise ValueError(f"The daq type {daq_type} is not available. "
+                             f"Allowed values are {list(daq_type_dict.keys())}")
+        else:
+            daq_type = daq_type_dict[daq_type]
+        if interp_method not in interp_method_dict:
+            raise ValueError(f"The interpolation method type {interp_method} is not available. "
+                             f"Allowed values are {list(interp_method_dict.keys())}")
+        else:
+            interp_method = interp_method_dict[interp_method]
+        if autosave_file_fmt in filefmt_dictionary:
+            filefmt_idx = filefmt_dictionary[autosave_file_fmt]
+        else:
+            raise ValueError("The file format is not valid. Valid formats are:  {}".format(list(filefmt_dictionary.keys())))
 
-    dev = PyQtziVirtualDevice(device_id)
-    print(dev.get_available_demods())
+        if module_name is None:
+            module_name = f'daq{len(self.daqmodules)}'
+
+        cmd_list = []
+        for demod, signals in demod_dictionary.items():
+            for sig in signals:
+                cmd_list.append(cmd_string.format(demod, sig))
+
+        flags = ziListEnum.recursive | ziListEnum.absolute | ziListEnum.streamingonly
+        streaming_nodes = self.ziServer.listNodes(self._baseaddress, flags)
+
+        daq_module = self.ziServer.dataAcquisitionModule()
+
+        for demod in demod_dictionary:
+            demod_path = self._baseaddress + f"/demods/{demod}/sample"
+            if demod_path.upper() not in streaming_nodes:
+                print(f'Device {self.dev_name} does not have the requested demods.')
+                raise Exception("Demodulator streaming nodes unavailable.")
+
+        if triggernode:
+            daq_module.set("triggernode", self._baseaddress + f"/{triggernode}")
+
+        sampling_rate = max([self.demod_properties[demod]['rate'] for demod in demod_dictionary])
+        num_cols = int(ceil(sampling_rate * burst_duration))
+        if daq_type == 0:
+            num_bursts = int(ceil(read_duration / burst_duration))
+            daq_module.set('count', num_bursts)
+        daq_module.set('device', self.dev_name)
+        daq_module.set('type', daq_type)
+        daq_module.set('grid/mode', interp_method)
+        daq_module.set('grid/cols', num_cols)
+        if interp_method != 4:
+            daq_module.set('duration', burst_duration)  # This parameter is read-only in exact acquisition
+
+        for keyword, value in kwargs.items():
+            daq_module.set(keyword, value)
+
+        if autosaving_file_path:
+            saving_directory = str(autosaving_file_path.parent)
+            filename = autosaving_file_path.name
+            daq_module.set('save/directory', saving_directory)
+            daq_module.set('save/filename', filename)
+            daq_module.set('save/fileformat', filefmt_idx)
+            daq_module.set('save/saveonread', True)
+        else:
+            daq_module.set('save/saveonread', False)
+
+        for requested_signal in cmd_list:
+            daq_module.subscribe(requested_signal)
+
+        self.daqmodules[module_name] = daq_module
+        self.daqmodules_sigs[module_name] = cmd_list
+
+        return module_name
+
+    def read_daq_module(self, daq_module_name, clear_after_finish=False):
+        daq_module = self.daqmodules[daq_module_name]
+        daq_module_signals = self.daqmodules_sigs[daq_module_name]
+
+        read_dictionary = daq_module.read(True)  # Return as a flat dictionary
+
+        out_dictionary = dict()
+        for signal in daq_module_signals:
+            if signal in read_dictionary:
+                signal_values = read_dictionary.pop(signal)
+                #signal = signal.replace(self._baseaddress, '')
+                data = np.zeros( (len(signal_values),) + signal_values[0]["timestamp"].shape)
+                timestamp = np.copy(data)
+                for ii, signal_value in enumerate(signal_values):
+                    data[ii] = signal_value["value"]
+                    timestamp[ii] = signal_value["timestamp"]
+            else:
+                timestamp, data = None, None
+            out_dictionary[signal] = [timestamp, data]
+
+        if clear_after_finish:
+            daq_module.pop(daq_module_name)
+            daq_module_signals.pop(daq_module_name)
+        return out_dictionary, read_dictionary
+
+    def get_subscribed_signals(self, daqmodule_name):
+        return [signal.replace(self._baseaddress, '') for signal in self.daqmodules_sigs[daqmodule_name]]
+
+class moddedPyQtziVirtualDevice(moddedziVirtualDevice, QObject):
+
+    signal_stop_daq = pyqtSignal()
+    signal_acquisition_completed = pyqtSignal()
+    def __init__(self, dev_name='', auto_properties=True, *args, **kwargs):
+        super(moddedPyQtziVirtualDevice, self).__init__(dev_name=dev_name, auto_properties=auto_properties, *args, **kwargs)
+
+        self.signal_stop_daq.connect(self.request_stop)
+
+
+
